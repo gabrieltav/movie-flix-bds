18ba4467064375cd5ee736095a399f4d67a484b1
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-03-04 14:32:52 -0400

    movie list

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,18 +1,51 @@
+import { AuthContext } from 'AuthContext';
+import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getTokenData, isAuthenticated } from 'util/auth';
+import history from 'util/history';
+import { removeAuthData } from 'util/storage';
 import './styles.css';
+
 const Navbar = () => {
+  const { authContextData, setAuthContextData } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthContextData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthContextData({
+        authenticated: false,
+      });
+    }
+  }, [setAuthContextData]);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthContextData({
+      authenticated: false,
+    });
+    history.replace('/');
+  };
+
   return (
-    <nav className=" navbar navbar-expand-md bg-primary main-nav">
-      <div className='container-fluid'>
-        <a href="link" className="nav-logo-text">
+    <nav className="navbar navbar-expand-md navbar-dark  bg-primary main-nav">
+      <div className="container-fluid">
+        <Link to="/" className="nav-logo-text">
           <h4>MovieFlix</h4>
-        </a>
-        <div className='colapse navbar-colapse '>
-          <ul className='navbar-nav offset-md-2'>
-            <li>
-              <a href="link" className='main-logof'>Sair</a>
-            </li>
-          </ul>
-        </div>
+        </Link>
+        {authContextData.authenticated && (
+          <div className="nav-logout">
+            <div className="nav-label">
+              <a href="#logout" onClick={handleLogoutClick}>
+                Sair
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
--- frontweb/src/pages/Movies/index.tsx
+++ frontweb/src/pages/Movies/index.tsx
@@ -1,11 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
 const Movies = () => {
-return (
-    <div>
-        <div>
-            <h1>Tela listagem de filmes</h1>
-        </div>
+  return (
+    <div className="movie-container">
+      <h1>Tela listagem de filmes</h1>
+      <div className="movie-list-container">
+        <ul>
+          <Link to="/movies/1">
+            <li>Acessar /movies/1</li>
+          </Link>
+          <Link to="/movies/2">
+            <li>Acessar /movies/2</li>
+          </Link>
+        </ul>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default Movies;
